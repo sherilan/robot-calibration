983ce78535f2d64dbeef8c6cae47062e1ca7e508
Sverre Herland <sverre.herland@ntnu.no>
2021-10-17 20:32:19 +0200

    Update experiment script

--- experiment.sh
+++ experiment.sh
@@ -3,7 +3,7 @@
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 N_RUNS=10
 
-TAG="err=1/5/2 noise=5"
+TAG="err=0.01-0.05-0.02_noise=0.005_n=500_moving"
 
 WAVE_DIR=$DIR/data/waves
 SIM_DIR=$DIR/data/results/$TAG
@@ -14,7 +14,7 @@ for wave_file in $(ls $WAVE_DIR); do
   echo "WAVE FILE: $wave_file"
   for i in $(seq $N_RUNS); do
     echo "RUN: $i"
-    run_dir="$SIM_DIR/$wave_file/run$i"
+    run_dir="$SIM_DIR/run$i"
     mkdir -p $run_dir
     # 0.01 -> 1cm, 0.05 -> ~3deg, 0.02 -> 2cm
     python $DIR/robot_calibration/sim.py $WAVE_DIR/$wave_file \
@@ -23,19 +23,20 @@ for wave_file in $(ls $WAVE_DIR); do
       --err_z_scale 0.01 \
       --err_a_scale 0.05 \
       --err_p_scale 0.02 \
-      --stationary \
       --speed 0.1 \
       --sim_data $run_dir/sim_data.txt \
       --sim_params $run_dir/sim_params.json
 
     python $DIR/robot_calibration/est.py $run_dir/sim_data.txt $run_dir/sim_params.json \
-      --wandb exposed-robocal \
+      --wandb robot-calibration \
       --tag $TAG \
       --out $run_dir/estimated_params.json \
-      --noise 0.005 \
-      --iters 10 \
+      --noise 0.000 \
+      --iters 25 \
+      --num 500 \
       --lr 1.0
   done
+  break
 done
 
 echo "DONE!"
