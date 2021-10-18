28f050e53a7f63c7c0dd37f08cd954f02da9443e
Sverre Herland <sverre.herland@ntnu.no>
2021-10-18 23:36:10 +0200

    Add calibration output

--- robot_calibration/__main__.py
+++ robot_calibration/__main__.py
@@ -4,6 +4,7 @@ import json
 import sys
 
 import numpy as np
+import pandas as pd
 
 import robot_calibration.est as est
 
@@ -30,7 +31,9 @@ parser.add_argument('--z0', default=0.0, type=float, help='Initial guess for z0 
 parser.add_argument('--a0', default=0.0, type=float, help='Initial guess for a0 in meters')
 parser.add_argument('--p0', default=20.0, type=float, help='Initial guess for p0 in meters')
 parser.add_argument('--lr', default=1.0, type=float, help='Optimizer step size')
-parser.add_argument('--iters', default=25, type=float, help='Number of optimization steps')
+parser.add_argument('--iters', default=25, type=int, help='Number of optimization steps')
+parser.add_argument('--repeats', default=1, type=int, help='Number of times to run estimation')
+parser.add_argument('--subsample', type=float, help='Bootstrap sampling of data for each repeat')
 parser.add_argument('--fmt', default='transforms', choices=['transforms', 'params'], help='Output format')
 parser.add_argument('--out', help='Output file (standard out if not provided)')
 
@@ -59,25 +62,55 @@ def main(args):
         )
         return 1
 
-    Tb = est.to_tensor(data[:, 0:16].reshape(-1, 4, 4))
-    Tr = est.to_tensor(data[:, 16:32].reshape(-1, 4, 4))
-    pen_xyz = est.to_tensor(data[:, 32:].reshape(-1, 3))
+    params = []
 
-    logger.info('Fitting parameters...')
-    estimator, final_loss = est.Estimator.fit(
-        data=(Tb, Tr, pen_xyz),
-        init=dict(
-            x0=args.x0,
-            y0=args.y0,
-            z0=args.z0,
-            a0=args.a0,
-            p0=args.p0,
-        ),
-        lr=args.lr,
-        iters=args.iters,
-        pbar=args.verbose,
-    )
-    logger.info('Finished fitting. Final loss: %s', final_loss)
+    for i in range(args.repeats):
+        logger.info('Iteration: %s', i + 1)
+
+        if args.subsample:
+            n = int(round(args.subsample * len(data)))
+            logger.info('Sampling %s random indices')
+            indices = np.random.choice(len(data), size=n)
+        else:
+            stride = len(data) // args.repeats
+            logger.info('Using data points [%s, %s)', i * stride, (i+1) * stride)
+            indices = np.arange(i * stride, (i + 1) * stride)
+
+
+        Tb = est.to_tensor(data[indices, 0:16].reshape(-1, 4, 4))
+        Tr = est.to_tensor(data[indices, 16:32].reshape(-1, 4, 4))
+        pen_xyz = est.to_tensor(data[indices, 32:].reshape(-1, 3))
+
+        logger.info('Fitting parameters...')
+        estimator, final_loss = est.Estimator.fit(
+            data=(Tb, Tr, pen_xyz),
+            init=dict(
+                x0=args.x0,
+                y0=args.y0,
+                z0=args.z0,
+                a0=args.a0,
+                p0=args.p0,
+            ),
+            lr=args.lr,
+            iters=args.iters,
+            pbar=args.verbose,
+        )
+        logger.info('Finished fitting. Final loss: %s', final_loss)
+        params.append(estimator.params)
+
+    if args.repeats > 0:
+        params = pd.DataFrame(params)
+        logger.info('Param Statistics: \n%s', params.describe())
+        params_mean = params.mean()
+        logger.info('Using mean params')
+        estimator = est.Estimator(
+            x0=params_mean.x,
+            y0=params_mean.y,
+            z0=params_mean.z,
+            a0=params_mean.a,
+            p0=params_mean.p,
+        )
+
     logger.info('Estimated params: \n%s', estimator.params)
     logger.info('Corresponding transforms: \n%s', estimator.transforms)
 
